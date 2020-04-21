07a2761fd0b59b29d09ededc770e8ea27ea535d9
rosendolu <rosendo2018@163.com>
2020-04-21 11:23:07 +0800

    chore

--- config.js
+++ config.js
@@ -1,7 +1,9 @@
 module.exports = {
 	dev: {
+		// mongodb:
+		// 	'mongodb://root:toor@cluster0-shard-00-00-fvlms.mongodb.net:27017,cluster0-shard-00-01-fvlms.mongodb.net:27017,cluster0-shard-00-02-fvlms.mongodb.net:27017/dev?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority',
 		mongodb:
-			'mongodb://root:toor@cluster0-shard-00-00-fvlms.mongodb.net:27017,cluster0-shard-00-01-fvlms.mongodb.net:27017,cluster0-shard-00-02-fvlms.mongodb.net:27017/dev?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority',
+			'mongodb://127.0.0.1:27017/?compressors=zlib&gssapiServiceName=mongodb',
 	},
 	test: {
 		mongodb:
