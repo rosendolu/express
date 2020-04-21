125e7cfe54207a43dcce9edffafb3d83a7a47e11
rosendolu <rosendo2018@163.com>
2020-04-21 17:36:17 +0800

    update config

--- config.js
+++ config.js
@@ -1,12 +1,6 @@
 module.exports = {
 	dev: {
-		// mongodb:
-		// 	'mongodb://root:toor@cluster0-shard-00-00-fvlms.mongodb.net:27017,cluster0-shard-00-01-fvlms.mongodb.net:27017,cluster0-shard-00-02-fvlms.mongodb.net:27017/dev?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority',
-		mongodb:
-			'mongodb://127.0.0.1:27017/?compressors=zlib&gssapiServiceName=mongodb',
+		mongodb: 'mongodb://127.0.0.1:27017/dev',
 	},
-	test: {
-		mongodb:
-			'mongodb://root:toor@cluster0-shard-00-00-fvlms.mongodb.net:27017,cluster0-shard-00-01-fvlms.mongodb.net:27017,cluster0-shard-00-02-fvlms.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority',
-	},
+	test: {},
 };
